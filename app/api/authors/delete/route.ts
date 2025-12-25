203156ce7485af7bac1c40f1c95cc1f05ac815f1
Osama Alwaly <160242270+OsamaGharibElwaly@users.noreply.github.com>
2025-12-25 16:05:34 +0200

    author fixing #2 admin panel

--- app/api/authors/delete/route.ts
+++ app/api/authors/delete/route.ts
@@ -1,20 +1,17 @@
-// pages/api/authors/delete.ts
-import { NextApiRequest, NextApiResponse } from 'next';
+import { NextRequest, NextResponse } from 'next/server';
 import prisma from '@/lib/db/client';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'POST') {
-    const { authorId } = req.body;
+export async function POST(req: NextRequest) {
+  try {
+    const { authorId } = await req.json();
 
-    try {
-      await prisma.author.delete({
-        where: { id: authorId },
-      });
-      return res.status(200).json({ message: 'Author deleted successfully' });
-    } catch (error) {
-      return res.status(500).json({ message: 'Failed to delete author' });
-    }
-  } else {
-    res.status(405).json({ message: 'Method Not Allowed' });
+    
+    await prisma.author.delete({
+      where: { id: authorId },
+    });
+
+    return NextResponse.json({ message: 'Author deleted successfully' });
+  } catch (error) {
+    return NextResponse.json({ message: 'Failed to delete author' }, { status: 500 });
   }
 }
